86896666d817bced08e130c2aa99ed1055f99d45
cheriejw <cheriejawoo@yahoo.com>
2016-10-18 22:13:29 -0700

    Fixed the minor error, working now

--- Server-Client Test/client2.php
+++ Server-Client Test/client2.php
@@ -4,16 +4,16 @@
     <script src="js/jquery-2.2.3.min.js"></script>
 
     <!-- <form action="serverSide.php" method="get" id="date"> -->
-    <form id="date"> 
+    <form id="date">
     <input type="text" name="StartDate" placeholder="YYYY-MM-DD"></input><br/>
     <input type="text" name="EndDate" placeholder="YYYY-MM-DD"></input><br/>
     <input type="submit" name="submit" value="Submit"></input>
     </form>
-    
-<script>   
+
+<script>
     var jsonFile;
-    $("date").submit(function() {
-    var mydata = $("date").serialize();
+    $("#date").submit(function() {
+    var mydata = $("#date").serialize();
     console.log(mydata); // it's only for test
     $.ajax({
         type: "GET",
@@ -28,11 +28,7 @@
             console.log("error");
         }
     });
-    return false;
 });
-
-    
-   
 </script>
 </body>
 </html>
--- Server-Client Test/serverSide.php
+++ Server-Client Test/serverSide.php
@@ -9,11 +9,11 @@
     $dbuser = "cecs491b1";
     $dbpass = "sh!oR2";
     $dbname = "cecs491bgroup";
-    
+
     $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-    //Test db connection 
+    //Test db connection
     if(mysqli_connect_errno()) {
-        die("Database connetion failed: ". 
+        die("Database connetion failed: ".
             mysqli_connect_error() .
             " (" . mysqli_connect_errno() . ")"
         );
@@ -26,26 +26,24 @@
     $query = sprintf("SELECT * FROM detections WHERE (Date_Time between '%s' and '%s');", $startDate, $endDate);
     echo $query;
     $result = mysqli_query($connection, $query); //$result is a 'resource' object
-    // Test if there was a query error 
+    // Test if there was a query error
     if(!$result) {
         die("Database query failed.");
     }
-?>
 
-<?php
     $row_array = array();
     // 3. User returned data (if any)
+    echo $result;
     while($row = mysqli_fetch_assoc($result)) {
         //output data from each row for DEBUGGING
         var_dump($row);
+        echo $row;
         echo "<hr />";
         //store each row in array to be return to client
         $row_array[] = $row;
     }
-?>
 
-<?php
-    // 4. Close database connection 
+    // 4. Close database connection
     mysqli_close($connection);
 
     // Send back json file
