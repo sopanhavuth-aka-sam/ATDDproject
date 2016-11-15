fa53b3d2423ade897ab6f5ec73c57e6d3521fad3
Cherie Woo <cheriejawoo@yahoo.com>
2016-11-15 14:25:44 -0800

    Quick starting steps to refactoring project. Turned code blocks into three functions.

--- atddScript.js
+++ atddScript.js
@@ -11,69 +11,57 @@
 *	https://select2.github.io/examples.html
 */
 
-/* config startdate picker
-* direction: false, means past only dates ending today
-* pair: $(enddate), sets the date of the enddate picker to startdate +1
-* offset shifts datepickers position relative to top right of the icon
+/*
+* This funciton setsup the start and end date picker.
 */
-$(document).ready(function() {
-	$("#startdate").Zebra_DatePicker({direction: false, pair: $(enddate), offset:[-190, 285]});
-});
-/* same code inside function body
-function setupStartdatePicker() {
+function setupDatePicker() {
+	/* config startdate picker : setupStartdatePicker() & config enddate picker : setupEnddatePicker()
+	* directionstartdate: false, means past only dates ending today
+	* pair: $(enddate), sets the date of the enddate picker to startdate +1
+	* direction for enddate: only allow dates in future from selected startdate
+	* offset shifts datepickers position relative to top right of the icon
+	*/
 	$(document).ready(function() {
 		$("#startdate").Zebra_DatePicker({direction: false, pair: $(enddate), offset:[-190, 285]});
-	});
-}
-*/
-
-/* config enddate picker
-* direction: only allow dates in future from selected startdate
-* offset shifts datepickers position relative to top right of the icon
-*/
-$(document).ready(function() {
-	$("#enddate").Zebra_DatePicker({direction: false, direction: 1, offset:[-190, 285]});
-});
-/* same code inside function body
-function setupEnddatePicker() {
-	$(document).ready(function() {
 		$("#enddate").Zebra_DatePicker({direction: false, direction: 1, offset:[-190, 285]});
 	});
-}
+	/* 
 */
+}
 
+function navbarButtons(){
+	// trackButton and detailsButton click functionality
+	$(document).ready(function() {
+		// tottle visability of the trackMenu when trackButton is clicked
+		$("#trackButton").click(function() {
+			$("#trackMenu").toggle("slow");
+		});
 
-// trackButton and detailsButton click functionality
-$(document).ready(function() {
-	// tottle visability of the trackMenu when trackButton is clicked
-	$("#trackButton").click(function() {
-		$("#trackMenu").toggle("slow");
+		// open user guide when detailsButton is clicked
+		$("#detailsButton").click(function() {
+			var url = "https://github.com/sopanhavuth-aka-sam/ATDDproject/blob/miguel/UserGuide.txt";
+			window.open(url, "_blank");
+		});
+	});
+}
+
+function loadingData(){
+	// parsing "Species data" into an array of objects (each row == object)
+	var speciesPromise = new Promise(function(resolve, reject) {
+		var dataset = [];	// array for species.csv data
+		d3.csv("Species.csv", function(data) {
+			dataset = data;
+			if(dataset.length > 0) {
+				resolve(dataset);	// successful data load, pass data to speciesPromise.then()
+			}
+			else {
+				reject("Parsing Specices Fail");	// failed to load data from .csv
+			}
+		});
 	});
 
-	// open user guide when detailsButton is clicked
-	$("#detailsButton").click(function() {
-		var url = "https://github.com/sopanhavuth-aka-sam/ATDDproject/blob/miguel/UserGuide.txt";
-		window.open(url, "_blank");
-	});
-});
-
-
-// parsing "Species data" into an array of objects (each row == object)
-var speciesPromise = new Promise(function(resolve, reject) {
-	var dataset = [];	// array for species.csv data
-	d3.csv("Species.csv", function(data) {
-		dataset = data;
-		if(dataset.length > 0) {
-			resolve(dataset);	// successful data load, pass data to speciesPromise.then()
-		}
-		else {
-			reject("Parsing Specices Fail");	// failed to load data from .csv
-		}
-	});
-});
-
-// if promise was resolve/successful
-speciesPromise.then(function(dataset) {
+	// if promise was resolve/successful
+	speciesPromise.then(function(dataset) {
 		var speciesOptions = [];
 		// load data for select box, key(id), value(string species_name)
 		for(i = 0; i < dataset.length; i++){
@@ -81,35 +69,36 @@ speciesPromise.then(function(dataset) {
 		}
 
 		// configure select species select box
-		$(document).ready(function() {
-			$("#speciesSlct").select2({
-			placeholder: "Select Species.",	// placeholder only works on IE v10 or higher (select2 docs)
-			allowClear: true,
-			data: speciesOptions,
+		$(document).ready(
+			function() {
+				$("#speciesSlct").select2({
+				placeholder: "Select Species.",	// placeholder only works on IE v10 or higher (select2 docs)
+				allowClear: true,
+				data: speciesOptions,
 			});
 
 			//DISPLAYING SPECIE INPUT WHEN 'GET VALUE' IS CLICKED                <----------------------------------
 			$(".getValue").click(function() {
-			input_specie= $(".slct1").val();									//SAVING INPUT AS A VALUE TYPE TO input_specie
+				input_specie= $(".slct1").val();									//SAVING INPUT AS A VALUE TYPE TO input_specie
 			});
 		});
 
-		/*** need to remove this 'individual' option for species ***/
-		$(document).ready(function() {
-			$("#individualSlct").select2({
-			placeholder: "Select Individual.",
-			allowClear: true,
-			data: speciesOptions,
-			maximumSelectionLength: 3
-			});
+			/*** need to remove this 'individual' option for species ***/
+			$(document).ready(function() {
+				$("#individualSlct").select2({
+				placeholder: "Select Individual.",
+				allowClear: true,
+				data: speciesOptions,
+				maximumSelectionLength: 3
+				});
 
-			//DISPLAYING INDIVIDUAL INPUT WHEN 'GET VALUE' IS CLICKED                <----------------------------------
-			$(".getValue").click(function() {
-			input_indiv= $(".slct2").val();										//SAVING INPUT AS A VALUE TYPE TO input_indiv
+				//DISPLAYING INDIVIDUAL INPUT WHEN 'GET VALUE' IS CLICKED                <----------------------------------
+				$(".getValue").click(function() {
+				input_indiv= $(".slct2").val();										//SAVING INPUT AS A VALUE TYPE TO input_indiv
+				});
 			});
-		});
-	}
-).catch(function(err) {
-		console.log(err);
-	}
-); // end speciesPromise
+		}).catch(function(err) {
+			console.log(err);
+		}
+	); // end speciesPromise	
+}
